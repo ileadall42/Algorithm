c97ef6e39ba277af34cabdbefd54e9d2b509ac5b
ileadall42 <595248227@qq.com>
2018-09-10 20:09:57 +0800

    Revert "Revert "the sec day to commit""
    This reverts commit 677f399e1223d2ad52f27e871d2c443498042930.

---
+++ DataStructure/ZJUC/Chapter2/LinkList.c
@@ -0,0 +1,29 @@
+//
+// Created by Mr.Tangfeifan on 2018/9/10.
+//
+
+#include <stdio.h>
+
+void getstart(int a);
+int main(int argc, char const *argv[])
+{
+    int a;
+    scanf("input a number %d", &a);
+
+    printf("haha,hello,world.the number is : %d", a);
+
+    for (size_t i = 0; i < a; i++)
+    {
+        printf("haha \n");
+    }
+
+    getstart(a);
+    return 0;
+}
+
+void getstart(int a)
+{
+
+    printf("This is a function and get start of this :%d", a);
+    return;
+}
